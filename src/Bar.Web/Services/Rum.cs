b2cf5d123a8568d97c6d1e4382254b32b230556e
Olaf Kober <olaf.kober@outlook.com>
2021-01-01 21:08:11 +0100

    refined

--- src/Bar.Web/Services/Rum.cs
+++ src/Bar.Web/Services/Rum.cs
@@ -27,10 +27,24 @@ using System;
 
 namespace Bar.Web.Services
 {
+    /// <summary>
+    /// Represents a Rum.
+    /// </summary>
     public sealed class Rum
     {
+        /// <summary>
+        /// The name of the Rum, e.g. "A.H. Riise X.O. Reserve".
+        /// </summary>
         public String Name { get; set; }
+
+        /// <summary>
+        /// A teaser text of the Rum, e.g. "Special Edition, Kong Haakon".
+        /// </summary>
         public String Teaser { get; set; }
+
+        /// <summary>
+        /// The image file name of the Rum, e.g. "KRO00442.jpg".
+        /// </summary>
         public String Image { get; set; }
     }
 }
