de903db5f453cb1ec16aa77f64690f891f76cb28
Olaf Kober <olaf.kober@outlook.com>
2021-01-24 10:11:15 +0100

    applied new code style

--- src/Bar.Web/Startup.cs
+++ src/Bar.Web/Startup.cs
@@ -57,9 +57,7 @@ namespace Bar.Web
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
             {
                 app.UseExceptionHandler("/Error");
